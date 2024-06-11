bab8a02a261c75a1f03b979da992bae41f3ac6db
Mohamed Elazzaoui <melazzaoui@insea.ac.ma>
2024-06-11 11:15:11 +0100

    refactor: update image paths in train_net.py

--- dit/object_detection/train_net.py
+++ dit/object_detection/train_net.py
@@ -73,15 +73,15 @@ def main(args):
     register_coco_instances(
         "icdar2019_train",
         {},
-        "data/train.json",
-        "data/train"
+        "/kaggle/working/data/train.json",
+        "/kaggle/working/data/train"
     )
 
     register_coco_instances(
         "icdar2019_test",
         {},
-        "data/test.json",
-        "data/test"
+        "/kaggle/working/data/test.json",
+        "/kaggle/working/data/test"
     )
 
     cfg = setup(args)
